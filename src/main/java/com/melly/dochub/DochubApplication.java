ff5b5f09a2a6bdf44c8b58589162eb5fab7bef82
haily <49448330+melly8954@users.noreply.github.com>
2025-12-05 08:33:56 +0900

    feat: init dochub

---
+++ src/main/java/com/melly/dochub/DochubApplication.java
@@ -0,0 +1,13 @@
+package com.melly.dochub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DochubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DochubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/melly/dochub/DochubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melly.dochub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DochubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
